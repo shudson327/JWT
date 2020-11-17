1dec59367f9701bf89e74d09906fddacd7344f37
Lashonda <lashondamiller007@gmail.com>
2020-11-17 10:55:24 -0500

    lesson 1

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://Lmiller007:OreoCookies11@jwt-lesson.wcoav.mongodb.net/JWT-Lesson?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
